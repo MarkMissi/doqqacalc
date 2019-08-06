0617e2f37377eb17a1fdde38995b539840dc70a1
Mark Missi <markiwoose@hotmail.com>
2019-08-06 18:04:26 +0300

    Switching xmlhttp request 'async' argument to true. Trying to avoid sending a blocking request on the main thread so the browser doesn't nag me.

--- scripts/FileReader.js
+++ scripts/FileReader.js
@@ -3,7 +3,7 @@ var filePath = "../res/damage/Damage_T.Gohan.csv";
 function loadFile(path) {
   var result = null;
   var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", path, false);
+  xmlhttp.open("GET", path, true);
   xmlhttp.send();
   if (xmlhttp.status==200) {
     result = xmlhttp.responseText;
